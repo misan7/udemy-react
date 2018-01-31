6faed9df076303cf2fc248463b74ea2d41f0b6f6
Sergio Morales <sergio.mt88@gmail.com>
2018-01-31 23:17:30 +0100

    3 ways to create a Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Tres maneras de crear un Componente:
+
+// 1a-
+// function Hello (props) {
+//   return <h1>{ props.title }</h1>
+// }
+
+// 2a-
+// const Hello = (props) => <h1>{ props.title }</h1>
+
+// 3a-
+class Hello extends Component {
+  render() {
+    return <h1>{ this.props.title }</h1>
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello  World</h1>
+          <Hello title='Hello from React' />
         </header>
         <p className="App-intro">Esto es un párrafo y <strong>esto es negrita</strong></p>
         <p>Esto es otro párrafo</p>
