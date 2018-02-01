d732d7bedba00c202ec12d8ff40e6a63d1a023df
Sergio Morales <sergio.mt88@gmail.com>
2018-02-01 23:05:00 +0100

    default props

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class Hello extends Component {
+class Title extends Component {
   render() {
-    return <h1>{ this.props.title }</h1>
+    return <h1>{this.props.text}</h1>
   }
 }
 
-class Text extends Component {
-  render() {
-    const {
-      arrayOfNumbers,
-      objectWIthInfo,
-      multiply,
-      title
-    } = this.props
-
-    const mappedNumbers = arrayOfNumbers.map(multiply)
-
-    return (
-      <div>
-        {title}
-        <p>{ mappedNumbers.join(', ') }</p>
-        <p>{ objectWIthInfo.key }</p>
-      </div>
-    )
-  }
+Title.defaultProps = {
+  text: 'Esto es un título por defecto'
 }
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Hello title='Hello from React' />
-        </header>
-        <Text
-          arrayOfNumbers={[2,5,6,10]}
-          objectWIthInfo={{ key:'value1', key2:'value2' }}
-          multiply={(number) => (number*4)}
-          title={<h1>Probando props con React</h1>}
-        />
-      </div>
-    );
+    return <Title/>
   }
 }
 
