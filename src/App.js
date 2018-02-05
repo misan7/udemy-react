afe5a0c5f1dc85a9955c63af0b7bf7a1bf47c314
Sergio Morales <sergio.mt88@gmail.com>
2018-02-05 22:10:17 +0100

    Render mediante Condicional

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
-import React, { Component } from 'react';
-
-// Información inmutable, solo actualizar con setState y es Asíncrono
-class Contador extends Component {
-  constructor(props) {
-    super(props)
-    console.log(this.props.contadorInicial)
-    this.state = { contador: this.props.contadorInicial }
-    setInterval(() => {
-      this.setState({ contador: this.state.contador + 1})
-    }, 1000)
-  }
-
-  render() {
-    return (
-      <ContadorNumero numero={this.state.contador}/>
-    )
-  }
-}
-
-Contador.defaultProps = {
-  contadorInicial:0
-}
-
-class ContadorNumero extends Component {
-  render() {
-    return <span>{this.props.numero}</span>
-  }
-}
+import React, { Component } from 'react'
+import ConditionalSection from './sections/conditional'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Propagando state en nuestros componentes</p>
-        <Contador contadorInicial={100}/>
+        <ConditionalSection />
       </div>
     )
-
   }
 }
 
-export default App;
+export default App
