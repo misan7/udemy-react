1dbb59226ced5965d20ac48f979a3c899904dcdb
Sergio Morales <sergio.mt88@gmail.com>
2018-02-06 23:13:21 +0100

    Array utilizando key y map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
-import ConditionalSection from './sections/conditional'
 
 class App extends Component {
   render() {
+    const numbers = [1, 1, 3, 4, 5, 6]
+
     return (
       <div className="App">
-        <ConditionalSection />
+        <h4>Lista utilizando map</h4>
+        {numbers.map((number, index) => {
+          return <p key={index}>Soy el número {number}</p>
+        })}
       </div>
     )
   }
