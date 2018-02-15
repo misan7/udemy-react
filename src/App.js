41b2b0bfa43c39fe3d3e730d354e33b81a050841
Sergio Morales <sergio.mt88@gmail.com>
2018-02-15 22:36:45 +0100

    Eventos: button

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react'
-import cars from './data/cars.json'
 
-class CarItem extends Component {
-  render() {
-    const { car } = this.props
-
-    return (
-      <li>
-        <p>Nombre: {car.manufacturer} {car.model}</p>
-        <p>Precio: {car.price}</p>
-        <a href={car.wiki} target='_blank'>Link to wiki</a>
-      </li>
-    )
-  }
-}
 class App extends Component {
   render() {
 
     return (
       <div className="App">
-        <h4>Lista con Objetos</h4>
-        <ul>
-        {
-          cars.map(car => {
-            return <CarItem key={car.wiki} car={car}/>
-          })
-        }
-        </ul>
+        <h4>Eventos</h4>
+        <button onClick={() => alert('Me has clicado :O')}>Haz click en mí!</button>
       </div>
     )
   }
