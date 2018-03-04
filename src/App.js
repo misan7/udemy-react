a9718ae22a278f233f71370919c2d350bd82ebef
Sergio Morales <sergio.mt88@gmail.com>
2018-03-04 21:33:05 +0100

    prop Children

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
-import Forms from './sections/forms'
+
+class Box extends Component {
+  render() {
+    return (
+      <div style={{ border: '1px solid cyan', margin: 5, padding: 5 }}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <div className="App">
-        <Forms />
+        <h4>Children props</h4>
+        <Box>Hola! Soy un Children!</Box>
+        <Box>Soy un children que adopta propiedades</Box>
+        <Box>Todos los Box de la app tendrán las mismas props</Box>
       </div>
     )
   }
