72b5d18c49ae790547e70b8381b798490eda671b
Sergio Morales <sergio.mt88@gmail.com>
2018-03-05 22:35:40 +0100

    Ciclo de Vida: Constructor

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-
-class Article extends Component {
-  static propTypes = {
-    author: PropTypes.string.isRequired
-  }
-  
-  render() {
-    const { author, children, date, title } = this.props
-
-    return (
-      <section style={{ borderBottom: '1px solid black', marginBottom: 50 }}>
-        <h2>{title}</h2>
-        {author && <p><em>Escrito por {author}</em></p>}
-        <date>{date}</date>
-        <article>
-          {children}
-        </article>
-      </section>
-    )
-  }
-}
 
 class App extends Component {
+  // constructor por defecto:
+  // constructor (...args) {
+  //   super(...args)
+  // }
+
+  constructor (props) {
+    super(props)  // Método que llama al constructor de la clase de la que extendemos, en este caso 'Component'
+    // inicializamos el state del componente con todas las variables que queramos
+    this.state = { mensajeInicial: 'Mensaje Inicial'}
+    // bindeamos el contexto al Método
+    // this.handleClick = this.handleClick.bind(this)
+  }
+
+  // Con arrow function es más limpio que con bind
+  handleClick = () => {
+    this.setState({ mensajeInicial: 'Mensaje Cambiado'})
+  }
+
   render() {
     return (
       <div className="App">
-        <h4>Children props</h4>
-        <Article
-          date={new Date().toLocaleDateString()}
-          title='Artículo sobre la prop children'
-        >
-          <p>El contenido que envolvemos dentro del componente Article sera enviado al componente como {this.props.children}</p>
-          <strong>Y mantiene las etiquetas y componentes añadidos dentro</strong>
-        </Article>
+        <h4>Ciclo de montaje: constructor</h4>
+        {this.state.mensajeInicial}
+        <button onClick={this.handleClick}>
+          Cambiar mensaje
+        </button>
       </div>
 
     )
