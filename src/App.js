1bd716e34a72169eafc6e235d5a94b3c2269eb5c
Sergio Morales <sergio.mt88@gmail.com>
2018-03-07 22:48:06 +0100

    Ciclo de Vida: componentWillMount

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import React, { Component } from 'react'
+import ComponentWillMount from './sections/life-cycle/componentWillMount.js'
 
 class App extends Component {
-  // constructor por defecto:
-  // constructor (...args) {
-  //   super(...args)
-  // }
-
-  constructor (props) {
-    super(props)  // Método que llama al constructor de la clase de la que extendemos, en este caso 'Component'
-    // inicializamos el state del componente con todas las variables que queramos
-    this.state = { mensajeInicial: 'Mensaje Inicial'}
-    // bindeamos el contexto al Método
-    // this.handleClick = this.handleClick.bind(this)
-  }
-
-  // Con arrow function es más limpio que con bind
-  handleClick = () => {
-    this.setState({ mensajeInicial: 'Mensaje Cambiado'})
-  }
-
-  render() {
+  render () {
     return (
       <div className="App">
-        <h4>Ciclo de montaje: constructor</h4>
-        {this.state.mensajeInicial}
-        <button onClick={this.handleClick}>
-          Cambiar mensaje
-        </button>
+        <ComponentWillMount />
       </div>
-
     )
   }
 }
